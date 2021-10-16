7ab23ec37cba6178a869628c54a25dfa83c211bc
Roman Bar <barom64@mail.ru>
2021-10-16 18:25:20 +0400

    Initial commit

---
+++ src/test/java/com/barom/mgz/MgzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barom.mgz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
